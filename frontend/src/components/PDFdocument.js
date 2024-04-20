a785981bcd29d7eb4c983bd28a82854abe93479f
Bhavye <bhavyedevelopment2003@gmail.com>
2024-04-20 11:40:32 +0530

    added download functionality for video and pdf

--- frontend/src/components/PDFdocument.js
+++ frontend/src/components/PDFdocument.js
@@ -8,6 +8,11 @@ const PDFDocument = ({ path }) => {
         title="PDF Viewer"
         className="w-[310px] h-[500px] overflow-hidden  border rounded-3xl scrollbar  border-gray-300"
       ></iframe>
+      <button className="mt-4 px-6 py-3 text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:bg-green-600 h-fit ml-5">
+        <a href={path} download={new URL(path).hostname}>
+          Download Video
+        </a>
+      </button>
     </div>
   );
 };
--- frontend/src/components/Video.js
+++ frontend/src/components/Video.js
@@ -13,6 +13,11 @@ const Video = ({ url }) => {
           <source src={url} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
+        <button className="mt-4 px-6 py-3 text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring focus:bg-green-600">
+          <a href={url} download={new URL(url).hostname}>
+            Download Video
+          </a>
+        </button>
       </React.Fragment>
     </div>
   );
